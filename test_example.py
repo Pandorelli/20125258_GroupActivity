bac347ea77f10003fe9b5b4224ae7600829aafa9
J167511 <J167511@tafe.wa.edu.au>
2025-05-08 10:18:42 +0800

    test: fix for assert result of test_subtract

--- test_example.py
+++ test_example.py
@@ -3,12 +3,14 @@ import example
 
 
 def test_subtract():
-    assert example.subtract(1, 2) == 3
+    assert example.subtract(1, 2) == -1
+
 
 def test_add():
     assert example.add(2, 3) == 5
     assert example.add('space', 'ship') == 'spaceship'
 
+
 # uncomment the following test in step 5
 def test_subtract1():
-   assert example.subtract(2, 3) == -1
+    assert example.subtract(2, 3) == -1
